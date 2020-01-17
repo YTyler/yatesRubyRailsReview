c94b476f4099daba3adc5f4278d3144b43e7c687
Tyler C. Yates <tyates907@gmail.com>
2020-01-17 12:00:53 -0800

    initialize project & create database

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# recipe_list = [
+#   [ "Oak", "Quercus" ],
+#   [ "Pine", "Pinus" ],
+#   [ "Sycamore", "Platanus" ],
+#   [ "Alder", "Alnus" ],
+#   [ "Birch", "Betula"],
+#   [ "Cherry", "Prunus"]
+# ]
+#
+# recipe_list.each do |name, category|
+#   Recipe.create( name: name, category: category )
+# end
