f640d0db16f548214d1a866d1a793f1365d3fd82
Tyler C. Yates <tyates907@gmail.com>
2020-01-17 16:54:28 -0800

    add controllers

--- db/migrate/20200115170050_create_products.rb
+++ db/migrate/20200115170050_create_products.rb
@@ -2,9 +2,8 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string(:name)
-      t.string(:category)
-      t.integer(:rank)
-      t.string(:instructions)
+      t.integer(:cost)
+      t.string(:country_of_origin)
 
       t.timestamps
     end
