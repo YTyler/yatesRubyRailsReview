192a84ed56ea4694fbeb27fc9569447bbe06bf31
Tyler C. Yates <tyates907@gmail.com>
2020-01-17 19:41:07 -0800

    add views

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,17 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :cost, presence: true
-  validates :origin, presence: true 
+  validates :origin, presence: true
+
+before_save(:format_input)
+
+private
+  def format_input
+    self.name = self.name.downcase
+    self.name = self.name.titleize
+
+    self.country_of_origin = self.country_of_origin.downcase
+    self.country_of_origin = self.country_of_origin.titleize
+
+  end
 end
