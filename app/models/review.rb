ac41666ef2c86e998d09ca06f6d5a8e12379b4d1
Tyler Yates <tyates907@gmail.com>
2020-01-20 15:14:46 -0800

    fix rating range

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,5 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates_length_of :content_body, minimum: 50, maximum: 250
   validates :rating, presence: true
-  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 5}
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6}
 end
