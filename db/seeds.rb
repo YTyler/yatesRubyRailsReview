c1ec06c433524ed3ccc83f0f6fd2a6d9c6dc506e
Tyler Yates <tyates907@gmail.com>
2020-01-20 15:10:14 -0800

    seed database (5 reviews per product)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  @product = Product.create!(name: Faker::Food.dish, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country)
+  5.times do |nested_index|
+    Review.create!(author: Faker::Artist.name, rating: Faker::Number.between(from: 1, to: 5), content_body: Faker::Lorem.paragraph_by_chars(number: 150, supplemental: false), product_id: @product.id)
+  end
+end
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
