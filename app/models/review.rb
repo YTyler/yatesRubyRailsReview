3bdbeda75a5cdedbff9d7905fc6ecb8245ce8cec
Tyler Yates <tyates907@gmail.com>
2020-01-20 13:19:12 -0800

    add validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
+  validates :author, format: { with: /[a-z\s]/i, message: "only allows letters" }
   validates :content_body, presence: true
-  validates :rating, presence: true 
+  validates_length_of :content_body, minimum: 50, maximum: 250
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 5}
 end
