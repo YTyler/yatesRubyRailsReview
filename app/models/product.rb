267b8bd90b105f0e1406a448e98ae3af4111ad54
Tyler Yates <tyates907@gmail.com>
2020-01-24 12:22:14 -0800

    add scope framework

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,19 @@
 class Product < ApplicationRecord
+  # Relations
   has_many :reviews, dependent: :destroy
+  # Validations
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
 
-before_save(:format_input)
+  # Scopes
+  scope :most_reviews, -> {}
+  scope :three_recent, -> { order(created_at: :desc).limit(3) }
+  scope :usa_made, -> { where}
+  # Callbacks
+  before_save(:format_input)
 
-private
+  private
   def format_input
     self.name = self.name.downcase
     self.name = self.name.titleize
